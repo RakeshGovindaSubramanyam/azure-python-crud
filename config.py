3c2512596135ea3b02222e1d8b53369155789d2b
Rakesh Govinda Subramanyam <govindasubramanyamrakesh1999@gmail.com>
2025-02-01 19:50:54 -0500

    updates

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 DATABASE_SERVER = os.getenv("python-crud-server.database.windows.net")
 DATABASE_NAME = os.getenv("python-crud-db")
 DATABASE_USERNAME = os.getenv("rakesh")
-DATABASE_PASSWORD = os.getenv("Raki@054")
+DATABASE_PASSWORD = os.getenv("python_crud_123")
 
 def get_connection_string():
-    return f"Driver={{ODBC Driver 17 for SQL Server}};Server=python-crud-server.database.windows.net;Database=python-crud-db;UID=rakesh;PWD=Raki@054"
+    return f"Driver={{ODBC Driver 17 for SQL Server}};Server=python-crud-server.database.windows.net;Database=python-crud-db;UID=rakesh;PWD=python_crud_123"
